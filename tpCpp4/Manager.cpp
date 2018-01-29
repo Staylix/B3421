be5ca85654ec544da2d540fd46007aeb9a6f26bf
Staylix <32682876+Staylix@users.noreply.github.com>
2018-01-29 22:09:55 +0100

    Construction du Manager
    Normalement ça compile ! (Warnings du au fait que ce n'est pas fini)

--- tpCpp4/Manager.cpp
+++ tpCpp4/Manager.cpp
@@ -13,6 +13,8 @@
 //-------------------------------------------------------- Include système
 using namespace std;
 #include <string>
+#include <iostream>
+
 //------------------------------------------------------ Include personnel
 #include "Manager.h"
 #include "logstream.h"
@@ -31,25 +33,113 @@ using namespace std;
 //////////////////////////////////////////////////////////////////  PUBLIC
 //---------------------------------------------------- Fonctions publiques
 
-
-int main(int argc, char *argv[])
-// Algorithme :
-//
+void test()
 {
     logstream logReader;
     logReader.open("anonyme.log");
 
-    Graph g;
+    Graph instGraph;
 
     int i = 0;
     while (i<20)
     {
         i++;
         log tempLog = *logReader.getLog();
-        g.add(tempLog.referer, tempLog.queryHit);
+        instGraph.add(tempLog.referer, tempLog.queryHit);
     }
-    g.createTop10();
-    g.createDotFile("Fichier.dot");
-    g.afficherMap();
+    instGraph.createTop10();
+    instGraph.createDotFile("Fichier.dot");
+    instGraph.afficherMap();
+}
+
+
+int main(int argc, char *argv[])
+// Algorithme :
+//
+{
+    test();
+
+// -------------------------- Initialisations
+    string logFile;
+    logstream logReader;
+    Graph instGraph;
+
+    if (argc >= 2)
+    {
+        logFile = argv[argc-1];
+        logReader.open(logFile);
+    }
+    else
+    {
+        cerr << "No enough argument" << endl;
+    }
+
+
+// -------------------------- Traitement de la commande
+
+    bool g = false;
+    bool e = false;
+    bool t = false;
+    string heure;
+    string dotFile;
+
+    for (int i = 1; i < argc-1; ++i)
+    {
+        string temp = argv[i];
+        if (temp == "-g")
+        {
+            g = true;
+            if (argc >= i+2)
+            {
+                dotFile = argv[++i];
+            }
+            else
+            {
+                cerr << "Dot File unspecified" << endl;
+            }
+        }
+        else if (temp == "-e")
+        {
+            e = true;
+        }
+        else if (temp == "-t")
+        {
+            t = true;
+            if (argc >= i+2)   // !!!! Format de l'heure pour les comparaisons ?
+            {
+                heure = argv[++i];
+            }
+            else
+            {
+                cerr << "Time unspecified" << endl;
+            }
+        }
+    }
+
+
+// -------------------------- Remplissage du graphe
+
+    if (logReader)
+    {
+        while (!logReader.eof())
+        {
+            log tempLog = *logReader.getLog();   // !!!!! Problème de pointeur !
+            instGraph.add(tempLog.referer, tempLog.queryHit);    // Ajouter les paramètres t et e !!!!!
+        }
+    }
+    else
+    {
+        cerr << "Error with file" << endl;
+    }
+
+
+// -------------------------- Executions
+    instGraph.createTop10();
+
+    if (g)
+    {
+        instGraph.createDotFile(dotFile);
+    }
+
 
 } //----- fin de Nom
