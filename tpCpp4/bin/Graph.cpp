d277dcdc6f1d551c5d00aefce1d1cffa8e8924f2
safiaelb <safia1346@gmail.com>
2018-02-05 12:23:48 +0200

    vf

---
+++ tpCpp4/bin/Graph.cpp
@@ -0,0 +1,145 @@
+﻿/*************************************************************************
+Graph  -  description
+-------------------
+d�but                : 09/01/2018
+copyright            : (C) 2018 par B3421
+e-mail               : safia.el-bayed@insa-lyon.fr
+gregoire.gentil@insa-lyon.fr
+*************************************************************************/
+
+//---------- R�alisation de la classe <Graph> (fichier Graph.cpp) ------------
+
+//---------------------------------------------------------------- INCLUDE
+
+//-------------------------------------------------------- Include syst�me
+using namespace std;
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+//------------------------------------------------------ Include personnel
+#include "Graph.h"
+
+//------------------------------------------------------------- Constantes
+
+//----------------------------------------------------------------- PUBLIC
+
+//----------------------------------------------------- M�thodes publiques
+// type Graph::M�thode ( liste des param�tres )
+// Algorithme :
+//
+//{
+//} //----- Fin de M�thode
+
+
+
+//Structure de données pour le graphe
+void Graph::addGraph(string ref, string hit)
+{
+	pair<unordered_map<string, int>::iterator, bool>  retour = graph.insert(make_pair(ref + " " + hit, 1));
+	if (!retour.second)
+	{
+		graph[ref + " " + hit] = graph[ref + " " + hit] + 1;
+	}
+}
+//Structure de données de passage pour le top10
+void Graph::addTop10(string ref, string hit) {
+	pair<unordered_map<string, int>::iterator, bool>  retour=passage.insert(make_pair(hit, 1));
+	if (!retour.second)
+	{
+		passage[hit] = passage[hit] + 1;
+	}
+	passage.insert(make_pair(ref, 0));
+}
+//Structure finale du top10
+void Graph::createTop10()
+{
+	for (unordered_map<string, int>::iterator it = passage.begin(); it != passage.end(); ++it)
+	{
+		top10.insert(make_pair(it->second, it->first));
+	}
+	
+}
+
+//Création du fichier dot
+bool Graph::createDotFile(string nomFichier)
+{
+	ofstream dot;
+	dot.open(nomFichier, std::ofstream::out | std::ofstream::trunc);
+	if (dot)
+	{
+		int i = 0;
+		dot << "digraph {\n";
+		for (unordered_map<string, int>::iterator it = passage.begin(); it != passage.end(); ++it)
+		{
+			dot << "node" << i << " [label=\"" << it->first << "\"];\n";
+			registry.insert(make_pair(it->first, i));
+			++i;
+		}
+		for (unordered_map<string, int>::iterator it = graph.begin(); it != graph.end(); ++it)
+		{
+			stringstream s(it->first);
+			string hit;
+			string referer;
+			getline(s, referer, ' ');
+			getline(s, hit);
+			int indexRef = registry[referer];
+			int indexHit = registry[hit];
+			string aecrire = "node";
+			aecrire += to_string(indexRef);
+			aecrire += " -> node";
+			aecrire += to_string(indexHit);
+			aecrire += " [label=\"";
+			aecrire += to_string(it->second);
+			aecrire += "\"];\n";
+			dot << aecrire;
+		}
+		dot.write("}", 1);
+		dot.close();
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+
+}
+
+//Affichage du top10
+void Graph::afficherTop10()
+{
+	int compteur = 0;
+	for (multimap<int, string>::reverse_iterator it = top10.rbegin(); it != top10.rend() && compteur< 10; ++it, ++compteur)
+	{
+		cout << it->second << " (" << it->first << " hits)" << endl;
+	}
+	if(compteur==0){
+		cout << "The ranking is empty."<<endl;
+	}
+}
+
+//-------------------------------------------- Constructeurs - destructeur
+
+Graph::Graph()
+// Algorithme :
+//
+{
+#ifdef MAP
+	cout << "Appel au constructeur de <Graph>" << endl;
+#endif
+} //----- Fin de Graph
+
+
+Graph::~Graph()
+// Algorithme :
+//
+{
+#ifdef MAP
+	cout << "Appel au destructeur de <Graph>" << endl;
+#endif
+} //----- Fin de ~Graph
+
+
+  //------------------------------------------------------------------ PRIVE
+
+  //----------------------------------------------------- M�thodes prot�g�es
---
+++ tpCpp4/bin/Manager.cpp
@@ -0,0 +1,203 @@
+/*************************************************************************
+Manager  -  description
+-------------------
+d�but                : 09/01/2018
+copyright            : (C) 2018 par B3421
+e-mail               : safia.el-bayed@insa-lyon.fr
+gregoire.gentil@insa-lyon.fr
+*************************************************************************/
+
+//---------- R�alisation du module <Manager> (fichier Manager.cpp) ---------------
+
+/////////////////////////////////////////////////////////////////  INCLUDE
+//-------------------------------------------------------- Include syst�me
+using namespace std;
+#include <string>
+#include <iostream>
+
+//------------------------------------------------------ Include personnel
+#include "Manager.h"
+#include "logstream.h"
+#include "Graph.h"
+
+///////////////////////////////////////////////////////////////////  PRIVE
+//------------------------------------------------------------- Constantes
+
+//------------------------------------------------------------------ Types
+
+//---------------------------------------------------- Variables statiques
+
+//------------------------------------------------------ Fonctions priv�es
+
+
+//////////////////////////////////////////////////////////////////  PUBLIC
+//---------------------------------------------------- Fonctions publiques
+
+
+
+int main(int argc, char *argv[])
+
+{
+	//cout << "nombre d'arguments " << argc << endl;
+	// -------------------------- Initialisations
+	string logFile;//string pour r�cup�rer le nom du fichier
+	logstream logReader;//fichier en entr�e
+	Graph instGraph;//objet graph
+	// A priori on ne demande aucune option
+	bool g = false;
+	bool e = false;
+	bool t = false;
+	int heureToInt;
+	string heure;//heure si choix de l'option
+	string dotFile;//nom du fichier dotfile si choix de l'option
+
+	if (argc < 2 || argc >7) // Si le nombre d'arguments est faux-->commande focement fausse
+	{
+		cerr << "Incorrect number of arguments." << endl;
+		return 3;
+	}
+	else // On traite la commande
+	{
+		logFile = argv[argc - 1];
+		if (logFile.length() >= 4 && logFile.substr(logFile.length() - 4).compare(".log")  && logFile.substr(logFile.length() - 4).compare(".txt"))
+		{
+			cerr << "The log file specified must be a .log or a .txt file." << endl;
+			return 5;
+		}
+		logReader.open(logFile);
+	}
+
+	if (logReader) // Nous sommes arriv�s � ouvrir le fichier
+	{
+
+	// -------------------------- Traitement de la commande
+
+
+
+		for (int i = 1; i < argc - 1; ++i)
+		{
+			string temp = argv[i];
+			if (temp == "-g" && !g)
+			{
+
+				g = true;
+				if (argc > i + 2) // si il y'a un nom de fichier dot suivi par au moins un argument
+				{
+					dotFile = argv[++i];
+					if (!(dotFile.find_first_of('/') == string::npos && dotFile.find_first_of('\\') == string::npos && dotFile.find_first_of(':') == string::npos && dotFile.find_first_of('?') == string::npos && dotFile.find_first_of('>') == string::npos && dotFile.find_first_of('<') == string::npos && dotFile.find_first_of('|') == string::npos && dotFile.find_first_of('"') == string::npos))
+					{
+						cerr << "Invalid character in the dot file name." << endl;
+						return 2;
+					}
+					else if (dotFile.find_last_of(".dot")!= (dotFile.length()-1))
+					{
+						cerr << "The dotfile specified must be a .dot." << endl;
+						return 6;
+					}
+				}
+				else
+				{
+					cerr << "Dot File unspecified." << endl;
+					return 10;
+				}
+			}
+			else if (temp == "-e" && !e)
+			{
+				e = true;
+
+			}
+			else if (temp == "-t" && !t)
+			{
+				t = true;
+				if (argc > i + 2)   // il y'a une heure suivie par au moins un argument
+				{
+					heure = argv[++i];
+					if (heure.length()==1 && int( heure.at(0))>=48 && int(heure.at(0))<=57)
+					{
+						 heureToInt= stoi(heure);
+					}
+					else if (heure.length() == 2 && int(heure.at(0)) >= 48 && int(heure.at(0)) <= 57 && int(heure.at(1)) >= 48 && int(heure.at(1)) <= 57)
+					{
+						heureToInt = stoi(heure);
+						if (heureToInt>23 || heureToInt <0)
+						{
+							cerr << "An hour must be between 0 and 23." << endl;
+							return 7;
+						}
+
+					}
+					else
+					{
+						cerr << "Invalid hour format." << endl;
+						return 8;
+					}
+				}
+				else
+				{
+					cerr << "The hour was not specified after the -t option." << endl;
+					return 11;
+				}
+			}
+
+			else
+			{
+				cerr << "Incorrect option." << endl;
+				return 4;
+			}
+		}
+
+
+		// -------------------------- Remplissage du graphe
+
+		if(t)
+		{
+			cout << " Warning : only hits between " << heureToInt << "h and " << heureToInt + 1 << "h have been taken into account." << endl;
+		}
+		if (e)
+		{
+			cout << "Warning : images, css and javascript files have been ignored." << endl;
+		}
+		
+		
+		
+
+		while (!logReader.eof())
+		{
+
+			logElement * tempLog = logReader.getLog(e,t,heure);   // !!!!! Probl�me de pointeur !
+			if (tempLog != nullptr && tempLog->queryType == "GET" /*&& tempLog->status.at(0) == '2'*/)
+			{
+				instGraph.addTop10(tempLog->referer.substr(0, tempLog->referer.find_first_of('?')), tempLog->queryHit.substr(0, tempLog->queryHit.find_first_of('?')));
+				if (g)
+				{ // On ne remplie la structure du graphe que si on a choisi l'option -g
+					instGraph.addGraph(tempLog->referer.substr(0, tempLog->referer.find_first_of('?')), tempLog->queryHit.substr(0, tempLog->queryHit.find_first_of('?')));
+				}
+			}
+		}
+	}	// end if (logReader)
+	else
+	{
+		cerr << "An error occured while trying to read your file, please make sure your file exists and is accessible." << endl;
+		return 9;
+	}
+
+
+	// -------------------------- Executions
+	instGraph.createTop10();
+	instGraph.afficherTop10();
+
+	if (g)
+	{
+		if (instGraph.createDotFile(dotFile))
+		{
+			cout << "Dot-file " << dotFile << " generated." << endl;
+		}
+		else
+		{
+			cerr << "Error during dot file creation." << endl;
+			return 12;
+		}
+	}
+
+	return 0;
+} //----- fin de Nom
